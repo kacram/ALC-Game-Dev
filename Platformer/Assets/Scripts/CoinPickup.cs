6ce11dda5a1d34cc076b637f3a3c665f668a698f
Unknown <20gfry@gmail.com>
2018-09-24 10:12:39 -0600

    added coin

---
+++ Platformer/Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour {
+
+    public int pointsToAdd;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.GetComponent<Rigidbody2D>() == null)
+            return;
+
+        ScoreManager.AddPoints(pointsToAdd);
+
+        Destroy(gameObject);
+    }
+}
