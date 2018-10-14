14203ffdcac6262bd0cf9872742cfb8f7c73b22f
Gavin Fry <20gfry@gmail.com>
2018-10-13 20:41:43 -0600

    Top Down Shooter

---
+++ Top Down Shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localRotation = new Quaternion(0f,0f, 57.2958f * Mathf.Atan2(transform.localPosition.y - Input.mousePosition.y,transform.localPosition.x - Input.mousePosition.x),0f);
+	}
+}
---
+++ Top Down Shooter/Assets/Scripts/PlayerFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour {
+
+    public Transform Player;
+
+    public float XOffset;
+    public float YOffset;
+    public float ZOffset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition = new Vector3(transform.localPosition.x + XOffset,transform.localPosition.y + YOffset,transform.localPosition.y + ZOffset);
+	}
+}
